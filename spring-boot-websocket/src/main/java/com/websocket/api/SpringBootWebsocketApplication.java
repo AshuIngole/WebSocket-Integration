e5c02669d149bf0ab0d41e34362ec756fd69974e
Admin <Admin@DESKTOP-QMFU2H6>
2023-12-08 18:03:02 +0530

    test websocket

---
+++ spring-boot-websocket/src/main/java/com/websocket/api/SpringBootWebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.websocket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//localhost:8080
+@SpringBootApplication
+public class SpringBootWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketApplication.class, args);
+	}
+
+}
+
